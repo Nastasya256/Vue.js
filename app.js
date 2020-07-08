af8a9e0a22ca4dcc65f20637afdc08b8173dc53b
Nastasya Zhivaeva <nastasyazzhh@gmail.com>
2020-07-09 00:47:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    title: 'Tasks for today',
+    newTodo: '',
+    todos: []
+  },
+  methods: {
+    addTodo(){
+      this.todos.push({
+        title: this.newTodo,
+        done: false
+      });
+      this.newTodo= '';
+    },
+    removeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    allDone(){
+      this.todos.forEach(todo => {
+        todo.done = true;
+      });
+
+    }
+  }
+  });
